8c0f0f0566a69134627bcc26f9f66314a8124f2f
thomasfork <thomasfork@gmail.com>
2021-08-03 21:47:04 -0700

    removed unused code

--- utils.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-def get_hostname():
-    hostname = subprocess.run(['hostname', '-I'], capture_output = True).stdout.decode().split(' ')[0]
-    return hostname
-
-if __name__ == '__main__':
-    get_hostname()
